1f33b03732e49607696c9d6a1e76f698d72d7901
Yevgeny York <yy@ixi.ru>
2020-03-22 18:39:11 +0300

    Start doing assignment 2 of week 3

---
+++ week3/assignment/solution_classes.py
@@ -0,0 +1,19 @@
+class CarBase:
+    def __init__(self, car_type=None, photo_file_name=None, brand=None, carrying=None):
+        self.car_type = car_type
+        self.photo_file_name = photo_file_name
+        self.brand = brand
+        self.carrying = carrying
+
+    def get_photo_file_ext():
+        pass
+
+class Car(CarBase):
+    def __init__(self):
+        pass
+
+
+class SpecMAchine(CarBase):
+    def __init__(self):
+        pass
+
